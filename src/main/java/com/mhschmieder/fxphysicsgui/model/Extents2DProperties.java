8041a946faed4192037048ad3f6b4269246ce889
Mark Schmieder <markhschmieder@gmail.com>
2025-10-24 23:48:37 -0400

    clean up most refactoring that IJ up

--- src/main/java/com/mhschmieder/fxphysicsgui/model/Extents2DProperties.java
+++ src/main/java/com/mhschmieder/fxphysicsgui/model/Extents2DProperties.java
@@ -21,12 +21,12 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *
- * This file is part of the FxPhysics Library.
+ * This file is part of the fxphysicsgui Library.
  *
- * You should have received a copy of the MIT License along with the FxPhysics
- * Library. If not, see <https://opensource.org/licenses/MIT>.
+ * You should have received a copy of the MIT License along with the
+ * fxphysicsgui Library. If not, see <https://opensource.org/licenses/MIT>.
  *
- * Project: https://github.com/mhschmieder/fxphysics
+ * Project: https://github.com/mhschmieder/fxphysicsgui
  */
 package com.mhschmieder.fxphysicsgui.model;
 
